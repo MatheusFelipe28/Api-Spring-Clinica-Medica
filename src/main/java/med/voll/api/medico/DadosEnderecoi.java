073fcc3c2fdb3a32db814c152e05694338dbdd8c
Matheus <matheusfelipegomesfernandes@gmail.com>
2023-07-09 22:49:13 -0300

    Integrando Repository

--- src/main/java/med/voll/api/medico/DadosEnderecoi.java
+++
@@ -1,5 +0,0 @@
-package med.voll.api.medico;
-
-public record DadosEnderecoi(String logradouro, String bairro, String cep, String cidade, String uf,
-                             String complemento, String numero) {
-}
